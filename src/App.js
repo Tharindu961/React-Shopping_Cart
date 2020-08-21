fe72cc989e16523cf0d9fdf24ce673e76a7d63e0
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-08-21 16:57:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,21 +19,13 @@ function App() {
   return (
     <div className="App">
       <h1>Products</h1>
-      <div>
+      { /*  <div>
         <h3>AA Battery</h3>
         <h4>$2.99</h4>
         <img src="https://exposupplies.co.uk/wp-content/uploads/100494.jpg" />
         <button>Add to Cart</button>
-      </div>
+      </div>  */}
 
-      <div>
-        <h3>Blanket</h3>
-        <h4>$19.99</h4>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQca4lKnePlg7EvPz5t1IDMQUltjBTGyjVIEA&usqp=CAU" />
-        <button>Add to Cart</button>
-      </div>
-      
-      
     </div>
   );
 }
